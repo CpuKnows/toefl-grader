734d0fcea5674d36a25631ab8d0834c82169fb7e
aalvarez <alvarez.aldoandres@gmail.com>
2018-04-19 19:46:41 -0500

    Updating executable folder

---
+++ executable/run.bat
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pip install nltk
+pip install numpy
+pip install regex
+pip install pandas
+pip install pyenchant
+pip install seaborn
+
+python grader_model_2.py
---
+++ executable/run_nlp_server.bat
@@ -0,0 +1,3 @@
+cd coreNLP
+# Run the server using all jars in the current directory (e.g., the CoreNLP home directory)
+java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 1500000
